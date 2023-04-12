8a1f1ee680f2b1a54a0a0ad34a87a4770ef39c92
João Victor Antonucci <joao.antonucci@abamais.com>
2023-04-11 23:48:33 -0300

    add win and lose condition

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -86,6 +86,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         if invaders.update(delta) {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
 
         // Draw & Render
         player.draw(&mut curr_frame);
@@ -96,6 +99,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+
+        // win or lose situation
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop;
+        }
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'gameloop;
+        }
     }
 
     // Cleanup
